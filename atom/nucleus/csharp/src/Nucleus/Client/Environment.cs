3620d68eb5252af4f03a0185d2c0aef0065bd1aa
Chandra Shekhar Paatni <chandrashekhar.paatni@tothenew.com>
2021-09-23 18:26:54 +0530

    C#

---
+++ atom/nucleus/csharp/src/Nucleus/Client/Environment.cs
@@ -0,0 +1,16 @@
+namespace Nucleus.Client
+{
+    public class EnvironmentEnum
+    {
+        public const string SANDBOX = "https://qa.hydrogenplatform.com";
+        public const string PRODUCTION = "https://qa.hydrogenplatform.com";
+    }
+
+
+
+    public enum EnumTest
+    {
+        SANDBOX,
+        PRODUCTION
+    }
+}
